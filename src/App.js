b46727b7e806a561ad5b4e1cb9ecb98c77467d25
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2022-09-18 19:37:11 +0500

    done with dark and light mode

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@ import './App.css';
 import Content from './components/content';
 import Sidebar from './components/sidebar';
 import Menubar from './components/menubar';
+import { useState } from 'react';
 
 const App = () => {
+
+  const [themeMode, setThemeMode] = useState("dark")
+  const toggleThemeMode = () => {
+    if (themeMode === "dark") {
+    setThemeMode("light")
+    document.body.style.backgroundColor = 'white';
+    }
+    else{
+      document.body.style.backgroundColor = 'black';
+    setThemeMode("dark")
+    }
+    // console.log(themeMode)
+  }
   return (
-    <div className="App">
+    <div className={`App ${themeMode==="dark"?"dark":"light"}`}  >
       <Menubar />
       <Content />
-      <Sidebar />
+      <Sidebar toggleThemeMode={toggleThemeMode} mode={themeMode}/>
     </div>
   );
 }
--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,7 +1,40 @@
-const Sidebar = () => {
+import { useState } from 'react';
+import { BsFillMoonFill, BsFillSunFill, BsSun } from "react-icons/bs";
+
+
+const Sidebar = (props) => {
+
+	// const [myStyle, setMyStyle] = useState({
+	// 	color: 'white',
+	// 	backgroundColor: ' rgb(21,32,43)'
+	// })
+	// const [btnText, setBtnText] = useState(<BsSun />)
+
+	// const themeMode = () => {
+		// if (props.mode==="dark") {
+		// 	setMyStyle({
+		// 		color: 'black',
+		// 		backgroundColor: 'white'
+		// 	})
+		// }
+		// else {
+		// 	setMyStyle({
+		// 		color: 'white',
+		// 		backgroundColor: ' rgb(21,32,43)'
+		// 	})
+		// }
+	// }
+	// <BsFillMoonFill/>
+	// <BsFillSunFill/>
+	// <BsSun/>
+	// <BsFillMoonFill/>
+	//style={myStyle}
 	return (
-		<div className="siderbar">
+		<div className="siderbar"  >
+			<button type="button" onClick={props.toggleThemeMode}>{(props.mode==="light")?<BsFillMoonFill/>:<BsSun />}</button>
 			<input type="search" placeholder="Search Twitter" />
+		
+			
 		</div>
 	);
 }
